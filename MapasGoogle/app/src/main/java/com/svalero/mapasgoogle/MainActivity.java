29ddcdaadbc4d5b1c395d7c047d71180d7a47701
Santi Stars <santi_r6@hotmail.es>
2022-02-22 18:04:43 +0100

    MapasGoogle terminado

--- MapasGoogle/app/src/main/java/com/svalero/mapasgoogle/MainActivity.java
+++ MapasGoogle/app/src/main/java/com/svalero/mapasgoogle/MainActivity.java
@@ -6,9 +6,12 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -17,13 +20,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerClickListener {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener,
+        GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerClickListener, LocationListener {
 
     private GoogleMap map;
-    private  Marker currentMarker;
-    private LocationManager locationManager;
-    private LocationProvider locationProvider;
+    private Marker currentMarker;
+    private LocationManager locationManager;    // Gestor de ubicaciones
+    private LocationProvider locationProvider;  // Proveedor de ubicaciones
+    private boolean drawRoute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,29 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(this);
+
+
+        FloatingActionButton mapButton = findViewById(R.id.map_button);
+        mapButton.setOnClickListener((view) -> {
+            if (drawRoute) {
+                locationManager.removeUpdates(this);
+                drawRoute = false;
+            } else {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                    return;
+                }
+                locationManager.requestLocationUpdates(locationProvider.getName(), 1000, 100, this);
+                drawRoute = true;
+            }
+        });
+        FloatingActionButton clearMapButton = findViewById(R.id.clear_map_button);
+        clearMapButton.setOnClickListener((view) -> {
+            map.clear();
+        });
     }
 
     @Override
@@ -52,22 +81,48 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         googleMap.setMyLocationEnabled(true);
 
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE) ;
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         locationProvider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
+        locationManager.requestLocationUpdates(locationProvider.getName(), 1000, 100, this);
+        drawRoute = true;
 
         loadHotels();
     }
 
-    private void loadHotels(){
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(location.getLatitude(), location.getLongitude()))
+                .snippet("Cosita fea!")
+                .title("Jenny"));
+    }
+
+    private void loadHotels() {
         // Cargo las ubis de la BBDD
+    }
+
+    private void getUserLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(locationProvider.getName());
+        location.getLongitude();
+        location.getLatitude();
     }
 
     @Override
     public void onMapClick(@NonNull LatLng latLng) {
         map.addMarker(new MarkerOptions()
                 .position(latLng)
-                .snippet("Ubicación")
-                .title("Título"));
+                .snippet("Cosita fea!")
+                .title("Jenny"));
     }
 
     @Override
@@ -77,8 +132,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public boolean onMarkerClick(@NonNull Marker marker) {
+        currentMarker = marker;
+
         String snippet = marker.getSnippet();
         Toast.makeText(this, snippet, Toast.LENGTH_LONG).show();
-        return true;
+        return false;
     }
 }
