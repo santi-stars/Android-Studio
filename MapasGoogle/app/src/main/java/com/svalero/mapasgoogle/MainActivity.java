e436023a5345be23bf31e723790cf5ccdefaa57e
Santi Stars <santi_r6@hotmail.es>
2022-02-22 16:03:38 +0100

    Marker en MapasGoogle

--- MapasGoogle/app/src/main/java/com/svalero/mapasgoogle/MainActivity.java
+++ MapasGoogle/app/src/main/java/com/svalero/mapasgoogle/MainActivity.java
@@ -1,12 +1,29 @@
 package com.svalero.mapasgoogle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerClickListener {
+
+    private GoogleMap map;
+    private  Marker currentMarker;
+    private LocationManager locationManager;
+    private LocationProvider locationProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +32,53 @@ public class MainActivity extends AppCompatActivity {
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+        googleMap.setOnMapClickListener(this);
+        googleMap.setOnMapLongClickListener(this);
+        googleMap.setOnMarkerClickListener(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE) ;
+        locationProvider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
+
+        loadHotels();
+    }
+
+    private void loadHotels(){
+        // Cargo las ubis de la BBDD
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+        map.addMarker(new MarkerOptions()
+                .position(latLng)
+                .snippet("Ubicación")
+                .title("Título"));
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+        String snippet = marker.getSnippet();
+        Toast.makeText(this, snippet, Toast.LENGTH_LONG).show();
+        return true;
     }
 }
